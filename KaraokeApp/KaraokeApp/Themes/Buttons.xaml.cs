b8102a6465d93a2fa0d1fffac9351c3fe518ea82
lgjoseluis <lgjoseluis@hotmail.com>
2022-06-01 19:37:21 -0500

    Cambio en la pantalla de inicio y se agrega estilo a controles

---
+++ KaraokeApp/KaraokeApp/Themes/Buttons.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KaraokeApp.Themes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Buttons : ResourceDictionary
+    {
+        public Buttons()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- KaraokeApp/KaraokeApp/ViewModels/MainPageViewModel.cs
+++ KaraokeApp/KaraokeApp/ViewModels/MainPageViewModel.cs
@@ -1,12 +1,42 @@
-﻿using Prism.Navigation;
+﻿using Prism.Commands;
+using Prism.Navigation;
+using Prism.Services;
 
 namespace KaraokeApp.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel(INavigationService navigationService) : base(navigationService)
+        private readonly IPageDialogService _pageDialog;
+
+        public DelegateCommand LoginCommand { get; private set; }
+        public DelegateCommand NewUserCommand { get; private set; }
+        public DelegateCommand RecoveryPasswordCommand { get; private set; }
+
+        public MainPageViewModel(INavigationService navigationService
+            , IPageDialogService pageDialog) : base(navigationService)
         {
             Title = "Página principal";
+
+            this._pageDialog = pageDialog;
+
+            this.NewUserCommand = new DelegateCommand(NewUser);
+            this.LoginCommand = new DelegateCommand(Login);
+            this.RecoveryPasswordCommand = new DelegateCommand(RecoveryPassword);
+        }
+
+        private void Login()
+        {
+            _pageDialog.DisplayAlertAsync("Info", "Evento click!", "Aceptar");
+        }
+
+        private void NewUser()
+        {
+            _pageDialog.DisplayAlertAsync("Info", "No implementado!", "Aceptar");
+        }
+
+        private void RecoveryPassword()
+        {
+            _pageDialog.DisplayAlertAsync("Info", "No implementado!", "Aceptar");
         }
     }
 }
